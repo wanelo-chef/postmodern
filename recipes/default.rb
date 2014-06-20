c6151fe3eec727f739dcff635f74b49f6d8a3a9e
Eric Saxby <sax@livinginthepast.org>
2014-06-20 15:52:41 -0700

    Install the pg gem manually with options on some platforms

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,14 @@ include_recipe 'build-essential'
 
 package node['postmodern']['ruby_package_name']
 
+helper = Postmodern::Helper.new(node)
+
+gem_package 'pg' do
+  options("-- #{helper.pg_options}")
+  gem_binary node['postmodern']['gem_binary']
+  not_if { helper.pg_options.empty? }
+end
+
 gem_package 'postmodern' do
   version node['postmodern']['version']
   gem_binary node['postmodern']['gem_binary']
